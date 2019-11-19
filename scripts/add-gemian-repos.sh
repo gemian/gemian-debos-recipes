8315c1a8027bf2c1b38de417c44043614af4df58
NeKit <nekit1000@gmail.com>
2019-11-19 12:15:00 +0100

    Attempt to fix rootfs building as signing key has expired

--- scripts/add-gemian-repos.sh
+++ scripts/add-gemian-repos.sh
@@ -3,7 +3,8 @@
 rm -f /etc/resolv.conf
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
 
-echo "deb http://gemian.thinkglobally.org/buster/ buster main" >> /etc/apt/sources.list.d/gemian.list
+echo "deb [trusted=yes] http://gemian.thinkglobally.org/buster/ buster main" >> /etc/apt/sources.list.d/gemian.list
+echo "deb [trusted=yes] http://gemian-buster.thinkglobally.org/buster/ buster main" >> /etc/apt/sources.list.d/gemian.list
 
 echo "" >> /etc/apt/preferences.d/gemian.pref
 echo "Package: *" >> /etc/apt/preferences.d/gemian.pref
