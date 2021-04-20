7100350773a9fd18092467b8467e11bec43dc387
Adam Boardman <adamboardman@gmail.com>
2021-04-20 13:16:08 +0100

    Updating to new server url to encourage fresh installs over upgrades, this is hopefully a one-time event

--- scripts/add-gemian-repos.sh
+++ scripts/add-gemian-repos.sh
@@ -4,7 +4,7 @@ rm -f /etc/resolv.conf
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
 
 echo "deb [trusted=yes] http://gemian.thinkglobally.org/buster/ buster main" >> /etc/apt/sources.list.d/gemian.list
-echo "deb [trusted=yes] http://gemian-buster.thinkglobally.org/buster/ buster main" >> /etc/apt/sources.list.d/gemian.list
+echo "deb [trusted=yes] http://gemian-planet.thinkglobally.org/buster/ buster main" >> /etc/apt/sources.list.d/gemian.list
 
 echo "" >> /etc/apt/preferences.d/gemian.pref
 echo "Package: *" >> /etc/apt/preferences.d/gemian.pref
@@ -13,7 +13,7 @@ echo "Pin-Priority: 1000" >> /etc/apt/preferences.d/gemian.pref
 
 echo "" >> /etc/apt/preferences.d/gemian.pref
 echo "Package: *" >> /etc/apt/preferences.d/gemian.pref
-echo "Pin: origin gemian-buster.thinkglobally.org" >> /etc/apt/preferences.d/gemian.pref
+echo "Pin: origin gemian-planet.thinkglobally.org" >> /etc/apt/preferences.d/gemian.pref
 echo "Pin-Priority: 2000" >> /etc/apt/preferences.d/gemian.pref
 
 curl https://gemian.thinkglobally.org/archive-key.asc | apt-key add -
