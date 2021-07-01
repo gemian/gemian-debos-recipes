42a9db876026290e5794928fd6841fcf10e01ecb
Adam Boardman <adamboardman@gmail.com>
2021-07-01 16:38:45 +0100

    Updated for bullseye based image creation - dropping graphics and android container for minimal image

--- scripts/add-gemian-repos.sh
+++ scripts/add-gemian-repos.sh
@@ -3,17 +3,11 @@
 rm -f /etc/resolv.conf
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
 
-echo "deb https://gemian.thinkglobally.org/buster/ buster main" >> /etc/apt/sources.list.d/gemian.list
-echo "deb https://gemian-planet.thinkglobally.org/buster/ buster main" >> /etc/apt/sources.list.d/gemian.list
-
-echo "" >> /etc/apt/preferences.d/gemian.pref
-echo "Package: *" >> /etc/apt/preferences.d/gemian.pref
-echo "Pin: origin gemian.thinkglobally.org" >> /etc/apt/preferences.d/gemian.pref
-echo "Pin-Priority: 1000" >> /etc/apt/preferences.d/gemian.pref
+echo "deb [trusted=yes] http://gemian-planet.thinkglobally.org/bullseye/ bullseye main" >> /etc/apt/sources.list.d/gemian.list
 
 echo "" >> /etc/apt/preferences.d/gemian.pref
 echo "Package: *" >> /etc/apt/preferences.d/gemian.pref
 echo "Pin: origin gemian-planet.thinkglobally.org" >> /etc/apt/preferences.d/gemian.pref
 echo "Pin-Priority: 2000" >> /etc/apt/preferences.d/gemian.pref
 
-curl https://gemian.thinkglobally.org/archive-key.asc | apt-key add -
+curl http://gemian.thinkglobally.org/archive-key.asc | apt-key add -
