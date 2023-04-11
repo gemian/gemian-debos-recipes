0f6550a0ec8c1c634b332dffdb79c2b9187a9fba
Adam Boardman <adamboardman@gmail.com>
2023-04-11 22:03:05 +0100

    Tidied up for bookworm

--- scripts/add-gemian-repos.sh
+++ scripts/add-gemian-repos.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
 rm -f /etc/resolv.conf
-echo "93.189.6.11 gemian-planet.thinkglobally.org" >> /etc/hosts
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
 
-echo "deb [trusted=yes] http://gemian-planet.thinkglobally.org/bullseye/ bullseye main" >> /etc/apt/sources.list.d/gemian.list
+echo "deb [trusted=yes] http://gemian.thinkglobally.org/bookworm/ bookworm main" >> /etc/apt/sources.list.d/gemian.list
 
 echo "" >> /etc/apt/preferences.d/gemian.pref
 echo "Package: *" >> /etc/apt/preferences.d/gemian.pref
-echo "Pin: origin gemian-planet.thinkglobally.org" >> /etc/apt/preferences.d/gemian.pref
+echo "Pin: origin gemian.thinkglobally.org" >> /etc/apt/preferences.d/gemian.pref
 echo "Pin-Priority: 2000" >> /etc/apt/preferences.d/gemian.pref
 
-curl https://gemian.thinkglobally.org/archive-key.asc | apt-key add -
